84ecd4f4a850a7c90b6e910b287b4a0316a6c0f0
czero1995 <czero1995@gmail.com>
2019-02-01 18:08:14 +0800

    test: vue-cli3.0

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,16 @@
 <template>
-    <div id="app">
-
-        <router-view/>
-
-    </div>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
-
 <script>
-import "../static/js/rem.js";
+import "../public/js/rem.js";
 export default {
-  name: "app",
-  created() {
-    // page load and read vuex state grom localStorage
-    localStorage.getItem("userMsg") &&
-      this.$store.replaceState(
-        Object.assign(
-          this.$store.state,
-          JSON.parse(localStorage.getItem("userMsg"))
-        )
-      );
-
-    //page refresh save vuex statw to localStorage
-    window.addEventListener("beforeunload", () => {
-      localStorage.setItem("userMsg", JSON.stringify(this.$store.state));
-    });
-  }
-};
+  
+}
 </script>
-
 <style lang="less">
-@import "../static/less/base.less";
+@import "../public/less/base.less";
 #app {
   height: 100%;
 }
